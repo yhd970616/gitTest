e5d63c8d0dc9ad1c08446e49867ece8337973a2b
yhd970616 <449646645@qq.com>
2019-10-25 15:54:21 +0800

    gitTest002

--- src/main/java/com/luobo/hello/helloWord001.java
+++ src/main/java/com/luobo/hello/helloWord001.java
@@ -1,6 +1,6 @@
 package com.luobo.hello;
 
-public class helloWord {
+public class helloWord001 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
---
+++ src/main/java/com/luobo/hello/helloWord002.java
@@ -0,0 +1,5 @@
+package com.luobo.hello;
+
+public class helloWord002 {
+
+}
